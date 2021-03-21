1cbd129ac8c0c422559613fa2bf53e7fb179c59e
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-03-21 18:48:43 +0200

    number parse error and design fixing

--- Code/helpers/csv.stream.helper.server.js
+++ Code/helpers/csv.stream.helper.server.js
@@ -25,14 +25,14 @@ module.exports.readHeader = async function (filePath) {
         return new Promise((resolve, reject) => {
             let fileHeaders = [];
             const readable = fs.createReadStream(filePath, {
-                encoding: 'latin1'
+                encoding: 'utf8'
             });
             // readable.setEncoding('ucs2');
 
             readable
                 .pipe(csv({
                     separator: ';',
-                    encoding: "latin1",
+                    encoding: "utf8",
                     // encoding: null
                     bom: true
                 }))
@@ -87,17 +87,27 @@ module.exports.importAccountCsvFile = async function (filePath, accountType = 1,
             let rowsToInsert = [];
 
             const parser = fs.createReadStream(filePath, {
-                    encoding: 'latin1'
+                    encoding: 'utf8'
                 })
                 .pipe(csv({
                     separator: ';',
-                    encoding: "latin1"
+                    encoding: "utf8"
                 }));
 
             for await (const row of parser) {
 
+                const accountNumber = decimalParser(row[template.accountNumber]);
+                if (isNaN(accountNumber)) {
+                    console.log(`${new Date()}: There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                    logger.error(`${new Date()}: There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                    
+                    reject(`There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                    return;
+                }
+                
+
                 rowsToInsert.push({
-                    accountNumber: decimalParser(row[template.accountNumber]),
+                    accountNumber: accountNumber,
                     companyCode: row[template.companyCode],
                     accountName: row[template.accountName],
                     accountType: AccountTypeEnum[accountType]
@@ -146,13 +156,13 @@ module.exports.importAccountCsvFile = async function (filePath, accountType = 1,
 
         } catch (err) {
             console.log("ERROR, the transaction will Rollback");
-            
+
             // const rgx = / at row (.*)/g;
             // const arr = rgx.exec(err.message);
             const splitedMsg = err.message.split(" at row ");
-            if(splitedMsg.length > 1 ) {
+            if (splitedMsg.length > 1) {
                 const rowNum = splitedMsg[1];
-                const theRealRowNum = rowNum > 0?  parseInt(rowNum) + 1 + (bulkCount * env.bulkInsertSize) : index;
+                const theRealRowNum = rowNum > 0 ? parseInt(rowNum) + 1 + (bulkCount * env.bulkInsertSize) : index;
                 const errorMsg = splitedMsg[0] + ' at row ' + theRealRowNum;
                 logger.error(`${new Date()}: ${splitedMsg[0]} at row ${theRealRowNum}`);
                 console.log("ERROR on row number: " + theRealRowNum);
@@ -161,7 +171,7 @@ module.exports.importAccountCsvFile = async function (filePath, accountType = 1,
                 console.log("ERROR on row number: " + index);
                 reject(err.message);
             }
-            
+
         }
     });
 };
@@ -193,11 +203,11 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
             let rowsToInsert = [];
 
             const parser = fs.createReadStream(filePath, {
-                    encoding: 'latin1'
+                    encoding: 'utf8'
                 })
                 .pipe(csv({
                     separator: ';',
-                    encoding: "latin1"
+                    encoding: "utf8"
                 }));
 
             for await (const row of parser) {
@@ -208,6 +218,13 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let accountName = row[Standardtemplate.accountName];
                 if (Standardtemplate.accountNumber && row[Standardtemplate.accountNumber]) {
                     accountNumber = decimalParser(row[Standardtemplate.accountNumber]);
+                    if (isNaN(accountNumber)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                        
+                        reject(`There is an ERROR on row ${index+1}, accountNumber/${Standardtemplate.accountNumber} should be number!`);
+                        return;
+                    }
                     if (accountNumber) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -228,6 +245,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let GLAccountName = null;
                 if (Standardtemplate.GLAccountNumber && row[Standardtemplate.GLAccountNumber]) {
                     GLAccountNumber = decimalParser(row[Standardtemplate.GLAccountNumber]);
+                    if (isNaN(GLAccountNumber)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, GLAccountNumber/${Standardtemplate.GLAccountNumber} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, GLAccountNumber/${Standardtemplate.GLAccountNumber} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, GLAccountNumber/${Standardtemplate.GLAccountNumber} should be number!`);
+                        return;
+                    }
                     if (GLAccountNumber) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -248,6 +271,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let contraAccountGLAccountName = null;
                 if (Standardtemplate.contraAccountGLAccountNo && row[Standardtemplate.contraAccountGLAccountNo]) {
                     contraAccountGLAccountNo = decimalParser(row[Standardtemplate.contraAccountGLAccountNo]);
+                    if (isNaN(contraAccountGLAccountNo)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountGLAccountNo/${Standardtemplate.contraAccountGLAccountNo} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountGLAccountNo/${Standardtemplate.contraAccountGLAccountNo} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, contraAccountGLAccountNo/${Standardtemplate.contraAccountGLAccountNo} should be number!`);
+                        return;
+                    }
                     if (contraAccountGLAccountNo) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -269,6 +298,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let debtorName = null;
                 if (Standardtemplate.debtorNumber && row[Standardtemplate.debtorNumber]) {
                     debtorNumber = decimalParser(row[Standardtemplate.debtorNumber]);
+                    if (isNaN(debtorNumber)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, debtorNumber/${Standardtemplate.debtorNumber} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, debtorNumber/${Standardtemplate.debtorNumber} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, debtorNumber/${Standardtemplate.debtorNumber} should be number!`);
+                        return;
+                    }
                     if (debtorNumber) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -289,6 +324,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let contraAccountDebtorName = null;
                 if (Standardtemplate.contraAccountDebtorNo && row[Standardtemplate.contraAccountDebtorNo]) {
                     contraAccountDebtorNo = decimalParser(row[Standardtemplate.contraAccountDebtorNo]);
+                    if (isNaN(contraAccountDebtorNo)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountDebtorNo/${Standardtemplate.contraAccountDebtorNo} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountDebtorNo/${Standardtemplate.contraAccountDebtorNo} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, contraAccountDebtorNo/${Standardtemplate.contraAccountDebtorNo} should be number!`);
+                        return;
+                    }
                     if (contraAccountDebtorNo) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -310,6 +351,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let creditorName = null;
                 if (Standardtemplate.creditorNumber && row[Standardtemplate.creditorNumber]) {
                     creditorNumber = decimalParser(row[Standardtemplate.creditorNumber]);
+                    if (isNaN(creditorNumber)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, creditorNumber/${Standardtemplate.creditorNumber} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, creditorNumber/${Standardtemplate.creditorNumber} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, creditorNumber/${Standardtemplate.creditorNumber} should be number!`);
+                        return;
+                    }
                     if (creditorNumber) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -332,6 +379,12 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 let contraAccountCreditorName = null;
                 if (Standardtemplate.contraAccountCreditorNo && row[Standardtemplate.contraAccountCreditorNo]) {
                     contraAccountCreditorNo = decimalParser(row[Standardtemplate.contraAccountCreditorNo]);
+                    if (isNaN(contraAccountCreditorNo)) {
+                        console.log(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountCreditorNo/${Standardtemplate.contraAccountCreditorNo} should be number!`);
+                        logger.error(`${new Date()}: There is an ERROR on row ${index+1}, contraAccountCreditorNo/${Standardtemplate.contraAccountCreditorNo} should be number!`);
+                        reject(`There is an ERROR on row ${index+1}, contraAccountCreditorNo/${Standardtemplate.contraAccountCreditorNo} should be number!`);
+                        return;
+                    }
                     if (contraAccountCreditorNo) {
                         let temp = await AccountModel.getAccounts().findAll({
                             where: {
@@ -348,13 +401,36 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                 }
 
 
+                const recordNumber = decimalParser(row[Standardtemplate.recordNumber]);
+                if (isNaN(recordNumber)) {
+                    console.log(`${new Date()}: There is an ERROR on row ${index+1}, recordNumber/${Standardtemplate.recordNumber} should be number!`);
+                    logger.error(`${new Date()}: There is an ERROR on row ${index+1}, recordNumber/${Standardtemplate.recordNumber} should be number!`);
+                    reject(`There is an ERROR on row ${index+1}, recordNumber/${Standardtemplate.recordNumber} should be number!`);
+                    return;
+                }
+                const creditAmount = decimalParser(row[Standardtemplate.creditAmount]);
+                if (isNaN(creditAmount)) {
+                    console.log(`${new Date()}: There is an ERROR on row ${index+1}, creditAmount/${Standardtemplate.creditAmount} should be number!`);
+                    logger.error(`${new Date()}: There is an ERROR on row ${index+1}, creditAmount/${Standardtemplate.creditAmount} should be number!`);
+                    reject(`There is an ERROR on row ${index+1}, creditAmount/${Standardtemplate.creditAmount} should be number!`);
+                    return;
+                }
+                const postingPeriod = decimalParser(row[Standardtemplate.postingPeriod]);
+                if (isNaN(postingPeriod)) {
+                    console.log(`${new Date()}: There is an ERROR on row ${index+1}, postingPeriod/${Standardtemplate.postingPeriod} should be number!`);
+                    logger.error(`${new Date()}: There is an ERROR on row ${index+1}, postingPeriod/${Standardtemplate.postingPeriod} should be number!`);
+                    reject(`There is an ERROR on row ${index+1}, postingPeriod/${Standardtemplate.postingPeriod} should be number!`);
+
+                    return;
+                }
+
                 rowsToInsert.push({
                     assignment: row[Standardtemplate.assignment],
                     documentNumber: row[Standardtemplate.documentNumber],
                     documentType: row[Standardtemplate.documentType],
                     documentDate: chrono.parseDate(row[Standardtemplate.documentDate]),
-                    recordNumber: decimalParser(row[Standardtemplate.recordNumber]),
-                    creditAmount: decimalParser(row[Standardtemplate.creditAmount]),
+                    recordNumber: recordNumber,
+                    creditAmount: creditAmount,
                     transactionCurrency: row[Standardtemplate.transactionCurrency],
                     applicationDocument: row[Standardtemplate.applicationDocument],
                     textPosting: row[Standardtemplate.textPosting],
@@ -362,7 +438,7 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
                     postingDate: chrono.parseDate(row[Standardtemplate.postingDate]),
                     companyCode: companyCode,
                     fiscalYear: row[Standardtemplate.fiscalYear],
-                    postingPeriod: decimalParser(row[Standardtemplate.postingPeriod]),
+                    postingPeriod: postingPeriod,
                     accountType: accountType,
                     accountNumber: accountNumber,
                     debitCredit: row[Standardtemplate.debitCredit],
@@ -429,15 +505,15 @@ module.exports.readCsvStream = async function (filePath, template = null, templa
             console.log("ERROR, the transaction will Rollback");
 
             const splitedMsg = err.message.split(" at row ");
-            if(splitedMsg.length > 1 ) {
+            if (splitedMsg.length > 1) {
                 const rowNum = splitedMsg[1];
-                const theRealRowNum = rowNum > 0?  parseInt(rowNum) + 1 + (bulkCount * env.bulkInsertSize) : index;
+                const theRealRowNum = rowNum > 0 ? parseInt(rowNum) + 1 + (bulkCount * env.bulkInsertSize) : index;
                 const errorMsg = splitedMsg[0] + ' at row ' + theRealRowNum;
                 logger.error(`${new Date()}: ${splitedMsg[0]} at row ${theRealRowNum}`);
                 console.log("ERROR on row number: " + theRealRowNum);
                 reject(errorMsg);
             } else {
-                console.log("ERROR on row number: " + index);
+                console.log("ERROR on row number: " + index+1);
                 reject(err.message);
             }
         }
