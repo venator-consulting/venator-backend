32b373b91a4215828411671922fb411e6fbb80e4
venator-consulting <53373977+Mohamad-Hamad91@users.noreply.github.com>
2021-03-27 20:13:31 +0100

    login component

--- Code/config/environment.js
+++ Code/config/environment.js
@@ -2,9 +2,9 @@ module.exports = {
     port: 8000,
     jwtSecret: 'IT\'s just a secret For JWT token so use some special @#$%!1',
     uploadPath: './modules/Admin/files/',
-    databaseName: 'accountant',
+    databaseName: 'venator',
     databaseUsername: 'root',
-    databasePassword: 'Asama56ab',
+    databasePassword: '',
     databaseHost: 'localhost',
     databaseDialect: 'mysql', /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
     bulkInsertSize: 2000,
