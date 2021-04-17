a4a78bdeca7b12799fdca0bf7960d1fc4884a466
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-04-17 22:21:27 +0300

    procedure management back-end

---
+++ Code/modules/Admin/controller/procedure.controller.server.js
@@ -0,0 +1,82 @@
+const procedureRepo = require('../../../repositories/procedure.repo.server');
+
+
+module.exports.fetchAll = async function (req, res, next) {
+    try {
+        const result = await procedureRepo
+            .fetchAll();
+        res
+            .status(200)
+            .json(result);
+    } catch (e) {
+        res
+            .status(500)
+            .json({
+                error: e
+            });
+    }
+};
+
+module.exports.fetchOne = async (req, res) => {
+    try {
+        const result = await procedureRepo
+            .fetchOne(req.params.id);
+        res
+            .status(200)
+            .json(result);
+    } catch (e) {
+        res
+            .status(500)
+            .json({
+                error: e
+            });
+    }
+};
+
+
+module.exports.insert = async function (req, res) {
+    try {
+        const procedure = req.body;
+        const result = await procedureRepo
+            .insert(procedure);
+        res.status(201)
+            .json(result);
+    } catch (e) {
+        res
+            .status(500)
+            .json({
+                error: e
+            });
+    }
+};
+
+module.exports.update = async function (req, res) {
+    try {
+        const procedure = req.body;
+        const result = await procedureRepo
+            .update(procedure, req.params.id);
+        res.status(201)
+            .json(result);
+    } catch (e) {
+        res
+            .status(500)
+            .json({
+                error: e
+            });
+    }
+};
+
+module.exports.delete = async function (req, res) {
+    try {
+        const result = await procedureRepo
+            .delete(req.params.id);
+        res.status(204)
+            .json(result);
+    } catch (e) {
+        res
+            .status(500)
+            .json({
+                error: e
+            });
+    }
+};
---
+++ Code/repositories/procedure.repo.server.js
@@ -0,0 +1,80 @@
+const Procedure = require('../models/procedures.model.server');
+
+
+module.exports.fetchAll = async () => {
+    try {
+        const res = await Procedure
+            .getProcedures()
+            .findAll({
+                where: {
+                    deleted: false
+                }
+            });
+        return res;
+    } catch (err) {
+        throw new Error('there_is_an_error_in_db_connection');
+    }
+};
+
+
+module.exports.fetchOne = async (id) => {
+    try {
+        const res = await Procedure
+            .getProcedures()
+            .findAll({
+                where: {
+                    deleted: false,
+                    id: id
+                }
+            });
+        return res;
+    } catch (err) {
+        throw new Error('there_is_an_error_in_db_connection');
+    }
+};
+
+
+
+module.exports.insert = async (model) => {
+        try {
+            const result = await Procedure
+                .getProcedures()
+                .create(model);
+            return result;
+        } catch (err) {
+            throw new Error('there_is_an_error_in_db_connection');
+        }
+}; // end of function
+
+
+
+module.exports.update = async (model, id) => {
+        try {
+            const result = await Procedure
+                .getProcedures()
+                .update(model, {
+                    where: {
+                        id: id
+                    }
+                });
+            return result;
+        } catch (err) {
+            throw new Error('there_is_an_error_in_db_connection');
+        }
+}; // end of update
+
+
+module.exports.delete = async (id) => {
+    try {
+        const result = await Procedure
+                .getProcedures()
+                .update({deleted: true}, {
+                    where: {
+                        id: id
+                    }
+                });
+            return result;
+    } catch (error) {
+        throw new Error('there_is_an_error_in_db_connection');
+    }
+};
