3fddf6b2510fa2354b7fddbc5cd1681c0d6df908
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-04-22 21:19:12 +0300

    organization modeladn migration to DB

---
+++ Code/models/organisation.model.server.js
@@ -0,0 +1,59 @@
+const {
+    DataTypes
+} = require('sequelize');
+const sequelize = require('../config/sequelize.config');
+
+const sequelizer = sequelize.getSequelize();
+
+
+
+const Organisation = sequelizer.define('Organisation', {
+    id: {
+        type: DataTypes.BIGINT(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING(50),
+    },
+    logo: {
+        type: DataTypes.STRING(250),
+    },
+    email: {
+        type: DataTypes.STRING(50),
+    },
+    country: {
+        type: DataTypes.STRING(25),
+    },
+    city: {
+        type: DataTypes.STRING(25),
+    },
+    postCode: {
+        type: DataTypes.STRING(25),
+    },
+    street: {
+        type: DataTypes.STRING(50),
+    },
+    houseNr: {
+        type: DataTypes.INTEGER,
+    },
+    deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, {
+    tableName: 'organisation'
+});
+
+
+module.exports.getOrganisation = function () {
+    return Organisation;
+};
+
+
+module.exports.syncOrganisation = async function () {
+    await this.getOrganisation().sync({
+        alter: true
+    });
+};
--- Code/modules/Admin/controller/data-migration.controller.server.js
+++ Code/modules/Admin/controller/data-migration.controller.server.js
@@ -1,23 +1,25 @@
-// const CountryModel = require('../../../models/country.model.server');
-// const CityModel = require('../../../models/city.model.server');
-// const StreetModel = require('../../../models/street.model.server');
-// const UserModel =  require('../../../models/user.model.server');
-// const AccountsModel = require('../../../models/accounts.model.server');
-// const { syncProcedures } = require('../../../models/procedures.model.server');
-
 module.exports.buildDatabaseSchema = async function (req, res, next) {
 
-    await require('../../../models/country.model.server').syncCountry();
-    await require('../../../models/city.model.server').syncCity();
-    await require('../../../models/street.model.server').syncStreet();
-    await require('../../../models/user.model.server').syncUser();
-    await require('../../../models/accounts.model.server').syncAccounts();
-    await require('../../../models/accountType.model.server').syncAccountType();
-    await require('../../../models/documentType.model.server').syncDocumentType();
-    await require('../../../models/posting.model.server').syncPosting();
-    await require('../../../models/procedures.model.server').syncProcedures();
-    await require('../../../models/role.model.server').syncRole();
-    res
-        .status(200)
-        .json('done');
+    try {
+        await Promise.all([
+            require('../../../models/country.model.server').syncCountry(),
+            require('../../../models/city.model.server').syncCity(),
+            require('../../../models/street.model.server').syncStreet(),
+            require('../../../models/user.model.server').syncUser(),
+            require('../../../models/accounts.model.server').syncAccounts(),
+            require('../../../models/accountType.model.server').syncAccountType(),
+            require('../../../models/documentType.model.server').syncDocumentType(),
+            require('../../../models/posting.model.server').syncPosting(),
+            require('../../../models/procedures.model.server').syncProcedures(),
+            require('../../../models/role.model.server').syncRole(),
+            require('../../../models/organisation.model.server').syncOrganisation()
+        ]);
+        res
+            .status(200)
+            .json('done');
+    } catch (error) {
+        res
+            .status(500)
+            .json(error);
+    }
 };
