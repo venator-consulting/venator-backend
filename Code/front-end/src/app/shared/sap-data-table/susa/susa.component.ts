cd1aada139bfff020526965c80b30c53489e51aa
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-06-12 10:44:44 +0300

    Susa export, spinner and loading table

--- Code/front-end/src/app/shared/sap-data-table/susa/susa.component.ts
+++ Code/front-end/src/app/shared/sap-data-table/susa/susa.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PostingDataService } from '../../service/posting-data.service';
 import { MessageService } from 'primeng/api';
 import { ProcedureService } from '../../service/procedure.service';
-import { DatePipe } from '@angular/common'
+import { DatePipe } from '@angular/common';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-susa',
@@ -13,6 +14,8 @@ import { DatePipe } from '@angular/common'
 })
 export class SusaComponent implements OnInit {
 
+  @ViewChild('dt') dt: ElementRef;
+  
   organisationId: number;
   procedureId: number;
   fromDate: Date = new Date();
@@ -20,11 +23,13 @@ export class SusaComponent implements OnInit {
   procedureName: string = "";
   cols: { header, field }[] = new Array();
   data: any[] = new Array();
+  waiting: boolean = false;
 
   constructor(private _messageService: MessageService, private _postingDataService: PostingDataService,
     private _router: Router, private prcService: ProcedureService, private datepipe: DatePipe) { }
 
   ngOnInit(): void {
+    this.waiting = true;
     this.organisationId = +localStorage.getItem('organisationId');
     this.procedureId = +localStorage.getItem('currentProcedureId');
 
@@ -35,8 +40,8 @@ export class SusaComponent implements OnInit {
         this.toDate = new Date(Date.parse(res[0].maxdate));
         let minYear = this.fromDate.getFullYear();
         debugger;
-        if(isNaN(minYear)) {
-          this.fromDate = this.toDate = new Date() ;
+        if (isNaN(minYear)) {
+          this.fromDate = this.toDate = new Date();
           this._messageService.add({
             severity: 'error',
             summary: 'ERROR',
@@ -49,7 +54,7 @@ export class SusaComponent implements OnInit {
       }, er => {
 
       });
-    
+
 
     if (this.procedureId && +this.procedureId > 0) {
       this.prcService
@@ -97,12 +102,14 @@ export class SusaComponent implements OnInit {
   } // end of ng on init
 
   getData() {
+    this.waiting = true;
     let fdate = this.datepipe.transform(this.fromDate, 'yyyy-MM-dd');
     let tdate = this.datepipe.transform(this.toDate, 'yyyy-MM-dd');
     this._postingDataService
       .getSusa(this.organisationId, this.procedureId, fdate, tdate)
       .subscribe(res => {
         this.data = res;
+        this.waiting = false;
       }, er => {
 
       });
@@ -112,4 +119,25 @@ export class SusaComponent implements OnInit {
     this._router.navigate(['/shared/data']);
   }
 
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.data);
+      const workbook = { Sheets: { 'susa': worksheet }, SheetNames: ['susa'] };
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      this.saveAsExcelFile(excelBuffer, "SuSa");
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    // const name = fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION;
+    // const file = new File([buffer], name, {type: EXCEL_TYPE});
+    const d: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    // FileSaver.saveAs(file);
+    FileSaver.saveAs(d, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
 }
