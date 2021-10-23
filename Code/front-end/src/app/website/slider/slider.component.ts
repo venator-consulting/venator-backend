c929b51972617dda46718dd2ee89fe001cbb6155
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-10-23 11:34:20 +0300

    slider fixing

--- Code/front-end/src/app/website/slider/slider.component.ts
+++ Code/front-end/src/app/website/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
+  @Output() Navigate = new EventEmitter();
+  @ViewChild('slider1') slider1;
+  @ViewChild('slider2') slider2;
+  @ViewChild('slider3') slider3;
+  slideNo: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  navigateTo(element: string) {
+    this.Navigate.emit(element)
+  }
+
+  slideLeft() {
+    this.slideNo--;
+    if(this.slideNo < 1) this.slideNo = 3;
+  }
+
+  slideRight() {
+    this.slideNo++;
+    if(this.slideNo > 3) this.slideNo = 1;
+  }
+
 
 }
