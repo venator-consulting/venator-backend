86b46351070744dbbffe8edfb3fd62f4c3a9b94d
Mohamad-Hamad91 <ite.mohamad.hamad@gmail.com>
2021-10-25 17:07:10 +0200

    file issus solved

--- Code/modules/Admin/controller/website.controller.server.js
+++ Code/modules/Admin/controller/website.controller.server.js
@@ -1,5 +1,6 @@
 const websiteRepo = require('../../../repositories/website.repo.server');
 const env = require('../../../config/environment');
+const path = require('path');
 
 module.exports.get = async (req, res) => {
     const result = await websiteRepo.fetchAll();
@@ -10,15 +11,15 @@ module.exports.updateSlider = async (req, res) => {
     const files = req.files;
     const data = JSON.parse(req.body.data);
     if (files[0]) {
-        data.sliderImg1 = env.domain + files[0].path;
+        data.sliderImg1 = env.domain + 'public/imgs/' + files[0].filename;
         // delete old first image
     }
     if (files[1]) {
-        data.sliderImg2 = env.domain + files[1].path;
+        data.sliderImg2 = env.domain + 'public/imgs/' + files[1].filename;
         // delete old first image
     }
     if (files[2]) {
-        data.sliderImg3 = env.domain + files[2].path;
+        data.sliderImg3 = env.domain + 'public/imgs/' + files[2].filename;
         // delete old first image
     }
     const result = await websiteRepo.update(data);
