ce7b6b2c3547b71ce2339e6644f7094c38fa6e6a
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2022-06-28 19:05:05 +0300

    catch any async error

--- Code/helpers/pdf.parser.js
+++ Code/helpers/pdf.parser.js
@@ -26,66 +26,75 @@ module.exports.parseAttacments = async function (orgId, prcId, res) {
     let count = 0;
     let bulkInsert = [];
     let attachment;
-    // for each attachment
-    for (let tempAttachment of attachments) {
-      attachment = tempAttachment;
-      if (fs.existsSync(attachment?.pstFilename))
-        try {
-          pdfParser.loadPDF(attachment?.pstFilename);
-        } catch (e) {
-          console.log(`Can't parse file ${attachment?.pstFilename}`);
-        }
-    }
-    // get text from pdf as json
-    pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
-    pdfParser.on("pdfParser_dataReady", async (pdfData) => {
-      // let array of records to bulk insert
-      bulkInsert = [];
-      // if text contains one of key words
-      // foreach keyword
-      for (let keyword of keywords) {
-        keyword = keyword.replace("like '%", "");
-        keyword = keyword.replace("%'", "");
-        keyword = keyword.replace("REGEXP '(\\b|[^a-zA-Z]+)", "");
-        keyword = keyword.replace("([^a-zA-Z]+|\\s*)'", "");
-        anotherKeyWord:
-        // foreach page
-        for (let page of pdfData.Pages) {
-          // foreach Text
-          for (let text of page.Texts) {
-            // foreach Row
-            for (let row of text.R) {
-              let rowText = row.T;
-              // if text contains keyword
-              if (rowText.includes(keyword)) {
-                // add attachment id, email id, keyword to bulk insert
-                bulkInsert.push({
-                  attachmentId: attachment?.id,
-                  emailHistoryId: attachment?.emailHistoryId,
-                  attachmentName: attachment?.originalName,
-                  keyword: keyword,
-                  procedureId: prcId
-                });
-                break anotherKeyWord;
-              }
-            } // end of foreach keyword
-          } // end of foreach Row
-        } // end of foreach Text
-      } // end of foreach page
-      // if there are records to insert
-      if (bulkInsert.length > 0) {
-        // insert records
-        await emailAttachmentKeywords
-          .getEmailAttachmentKeywords('email_attachment_keywords_' + orgId)
-          .bulkCreate(bulkInsert);
-        bulkInsert = [];
+    try {
+      // for each attachment
+      for (let tempAttachment of attachments) {
+        attachment = tempAttachment;
+        if (fs.existsSync(attachment?.pstFilename))
+          pdfParser.loadPDF(attachment?.pstFilename).catch(err => {
+            console.error(`Can't parse file ${attachment?.pstFilename}`);
+          });
+
       }
-      count++;
-      let progress = count / attachments.length * 100;
-      console.log(`progress ${progress}%...`);
-      res.write('event:' + 'progress\n');
-      res.write('data:' + JSON.stringify({ progress }) + '\n\n');
-    });
+      // get text from pdf as json
+      // pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError)).catch(err => {
+      //   console.error('Error caught', err);
+      // });
+      pdfParser.on("pdfParser_dataReady", async (pdfData) => {
+        // let array of records to bulk insert
+        bulkInsert = [];
+        // if text contains one of key words
+        // foreach keyword
+        for (let keyword of keywords) {
+          keyword = keyword.replace("like '%", "");
+          keyword = keyword.replace("%'", "");
+          keyword = keyword.replace("REGEXP '(\\b|[^a-zA-Z]+)", "");
+          keyword = keyword.replace("([^a-zA-Z]+|\\s*)'", "");
+          anotherKeyWord:
+          // foreach page
+          for (let page of pdfData.Pages) {
+            // foreach Text
+            for (let text of page.Texts) {
+              // foreach Row
+              for (let row of text.R) {
+                let rowText = row.T;
+                // if text contains keyword
+                if (rowText.includes(keyword)) {
+                  // add attachment id, email id, keyword to bulk insert
+                  bulkInsert.push({
+                    attachmentId: attachment?.id,
+                    emailHistoryId: attachment?.emailHistoryId,
+                    attachmentName: attachment?.originalName,
+                    keyword: keyword,
+                    procedureId: prcId
+                  });
+                  break anotherKeyWord;
+                }
+              } // end of foreach keyword
+            } // end of foreach Row
+          } // end of foreach Text
+        } // end of foreach page
+        // if there are records to insert
+        if (bulkInsert.length > 0) {
+          // insert records
+          await emailAttachmentKeywords
+            .getEmailAttachmentKeywords('email_attachment_keywords_' + orgId)
+            .bulkCreate(bulkInsert);
+          bulkInsert = [];
+        }
+        count++;
+        let progress = count / attachments.length * 100;
+        console.log(`progress ${progress}%...`);
+        res.write('event:' + 'progress\n');
+        res.write('data:' + JSON.stringify({ progress }) + '\n\n');
+      }).catch(err => {
+        console.error('Error caught', err);
+      });
+    } catch (e) {
+      console.log(`Can't parse file ${attachment?.pstFilename}`);
+    }
     resolve('done');
+  }).catch(err => {
+    console.error('Error caught', err);
   });
 };
